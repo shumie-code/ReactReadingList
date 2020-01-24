37d8a8ac56543fdbbaa94ec454106a5d0b5a8c4b
Petros Shumie <shumie.peter@gmail.com>
2020-01-24 10:22:14 -0500

    React reading list prepared for heroku deployment.

---
+++ Solved/client/build/precache-manifest.de2d8135d3a2a763a1bc584e460b69f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e77192c3aa00b3e7f6ab",
+    "url": "/static/js/main.5e0034bd.chunk.js"
+  },
+  {
+    "revision": "f2179e7f22e6577b35cf",
+    "url": "/static/js/2.565b7cae.chunk.js"
+  },
+  {
+    "revision": "e77192c3aa00b3e7f6ab",
+    "url": "/static/css/main.35826ef6.chunk.css"
+  },
+  {
+    "revision": "1064cc0dcab7a1cc3ca1f49b9ae95ce1",
+    "url": "/index.html"
+  }
+];
